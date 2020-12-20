6bc6d56a565d29b85f25d8c7b711472450bf51f7
grantsewell <grantsewell@gmail.com>
2020-12-19 19:35:46 -0500

    Update debian_install.sh

--- debian_install.sh
+++ debian_install.sh
@@ -3,6 +3,7 @@
 # Author: Grant Sewell
 # Date:   12/19/2020
 
+# Update cache
 apt-get update
 
 # Remove VIM Tiny
@@ -13,26 +14,33 @@ apt-get install vim htop open-vm-tools sudo nfs-kernel-server unzip ntfs-3g git 
 
 # Blacklist Unnecessary Functions for ESXi
 cat >/etc/modprobe.d/blacklist.conf <<EOL
-line 1, #Optimized Blacklist for Debian VM
-line 2, blacklist floppy
-line 3, blacklist mptctl
-line 4, blacklist pcspkr
-line 5, blacklist snd_pcm
-line 6, blacklist snd_page_alloc
-line 7, blacklist snd_timer
-line 8, blacklist snd
-line 9, blacklist soundcore
-line 10, blacklist coretemp
-line 11, blacklist parport
-line 12, blacklist parport_pc
-line 13, blacklist i2c-piix4
-... 
+#Optimized Blacklist for Debian VM
+blacklist floppy
+blacklist mptctl
+blacklist pcspkr
+blacklist snd_pcm
+blacklist snd_page_alloc
+blacklist snd_timer
+blacklist snd
+blacklist soundcore
+blacklist coretemp
+blacklist parport
+blacklist parport_pc
+blacklist i2c-piix4
 EOL
 
 # Disable IPv6
-echo '# Disable IPv6' >> /etc/sysctl.conf
-echo '#' >> /etc/sysctl.conf
-echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
-echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
-echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
-echo 'net.ipv6.conf.eth0.disable_ipv6 = 1' >> /etc/sysctl.conf
+cat >>/etc/sysctl.conf <<EOL
+# Disable IPv6
+#
+net.ipv6.conf.all.disable_ipv6 = 1
+net.ipv6.conf.default.disable_ipv6 = 1
+net.ipv6.conf.lo.disable_ipv6 = 1
+net.ipv6.conf.eth0.disable_ipv6 = 1
+EOL
+
+# Add User to Sudoers
+echo "Enter username to add to sudoers:"
+read user_sudo
+echo Adding $user_sudo to sudo group
+adduser $user_sudo sudo
