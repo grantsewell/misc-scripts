98954ca6fcb160486cb5ea52166c1cc0c6a66f08
grantsewell <grantsewell@gmail.com>
2020-12-19 19:09:52 -0500

    Create debian_install.sh
    v1.0 Debian virtual machine install script

---
+++ debian_install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Title:  Debian VM Build Script
+# Author: Grant Sewell
+# Date:   12/19/2020
+
+# Remove VIM Tiny
+apt-get remove vim-tiny
+
+# Install Standard Utilities
+apt-get install vim htop open-vm-tools sudo nfs-kernel-server unzip ntfs-3g git openssh-server apt-transport-https curl gnupg2 unzip
+
+# Blacklist Unnecessary Functions for ESXi
+cat >/etc/modprobe.d/blacklist.conf <<EOL
+line 1, #Optimized Blacklist for Debian VM
+line 2, blacklist floppy
+line 3, blacklist mptctl
+line 4, blacklist pcspkr
+line 5, blacklist snd_pcm
+line 6, blacklist snd_page_alloc
+line 7, blacklist snd_timer
+line 8, blacklist snd
+line 9, blacklist soundcore
+line 10, blacklist coretemp
+line 11, blacklist parport
+line 12, blacklist parport_pc
+line 13, blacklist i2c-piix4
+... 
+EOL
+
+# Disable IPv6
+echo '# Disable IPv6' >> /etc/sysctl.conf
+echo '#' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.lo.disable_ipv6 = 1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.eth0.disable_ipv6 = 1' >> /etc/sysctl.conf
