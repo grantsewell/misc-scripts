2d8b3ab60d6415d1e39e0f85488faf0b1e26ff0b
grantsewell <grantsewell@gmail.com>
2022-09-03 16:11:45 -0400

    Update okta_expire.sh

--- okta_expire.sh
+++ okta_expire.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-# Expire Okta Password v1.0
-# Expires an Okta account password immediate. User session is not terminated and temp password not assigned.
-# At next login or session expiration, user will be prompted for a password reset.
+# Title:  Expire Okta Password v1.0
+# Author: Grant Sewell
+# Date:   09/03/2022
+# Desc:   Expires an Okta account password immediate. User session is not terminated and temp password not assigned.
+#         At next login or session expiration, user will be prompted for a password reset.
 
 OktaApiToken=YOUR_API_TOKEN #https://developer.okta.com/docs/guides/create-an-api-token/main/
 OktaDomain=YOUR_OKTA_DOMAIN #https://mydomain.okta.com
@@ -9,18 +11,23 @@ OktaDomain=YOUR_OKTA_DOMAIN #https://mydomain.okta.com
 # Ask which user
 read -p "Enter user login/email address: " OktaUser
 
+# Query the user and retreive the base query URL
 OktaJSON=$(curl -s -X GET \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -H "Authorization: SSWS $OktaApiToken" \
 "https://$OktaDomain/api/v1/users?search=profile.login%20eq%20%22$OktaUser%22")
 
+# Parse the base query URL for the target user
 OktaIDURL=$(echo "$OktaJSON" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_%:-]*")
 
+# Expire the password
 curl -s -X POST \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -H "Authorization: SSWS $OktaApiToken" \
 "$OktaIDURL/lifecycle/expire_password?tempPassword=false" > /dev/null
 
+# Display confirmation, exit script
 echo "Password Expired for $OktaUser"
+exit 0
